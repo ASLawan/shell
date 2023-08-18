37af801248c4996bfc9da062c868ec91cbb18ff1
ASLawan <nawalnitsua@gmail.com>
2023-08-18 16:48:13 +0100

    Add variables

---
+++ shell_scripts/my_variables.sh
@@ -0,0 +1,7 @@
+#1/bin/bash
+
+name=Lawan
+town=Limbe
+
+echo "My name is $name"
+echo "I live in $town"
---
+++ shell_scripts/variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# sytem variables
+echo $USER
+echo $HOME
+echo $PWD
+echo $BASH
+echo $BASH_VERSION
